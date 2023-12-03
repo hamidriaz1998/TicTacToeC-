6b9775a15befb74b695213e19d71f3c23b138ef8
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-12-03 09:07:06 +0500

    Declare Functions

---
+++ game.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+bool isWinner(char,char[][3]);
+bool checkRows(char[][3],char);
+bool checkColumns(char[][3],char);
+bool checkDiagonal(char[][3],char);
+void printBoard(char[][3]);
+int main(){
+
+}
