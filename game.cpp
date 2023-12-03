64d722786a437dfe901f0c890c83469ee4f05260
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-12-03 09:08:48 +0500

    Write printBoard function

--- game.cpp
+++ game.cpp
@@ -8,3 +8,12 @@ void printBoard(char[][3]);
 int main(){
 
 }
+
+void printBoard(char board[][3]){
+    for (int i = 0;i<3;i++){
+        for (int j=0;j<3;j++){
+            cout<<board[i][j]<<"\t";
+        }
+        cout<<endl;
+    }
+}
