4ba826a9f4bddc0fa46f57bc409fa2d1225cfce2
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2023-12-03 09:16:06 +0500

    Write isFreeSpace() and registerMove()

--- game.cpp
+++ game.cpp
@@ -1,19 +1,45 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-bool isWinner(char,char[][3]);
-bool checkRows(char[][3],char);
-bool checkColumns(char[][3],char);
-bool checkDiagonal(char[][3],char);
+bool isWinner(char, char[][3]);
+bool checkRows(char[][3], char);
+bool checkColumns(char[][3], char);
+bool checkDiagonal(char[][3], char);
 void printBoard(char[][3]);
-int main(){
-
+void registerMove(char[][3], char, string);
+bool isFreeSpace(char[][3], string move);
+int main()
+{
 }
 
-void printBoard(char board[][3]){
-    for (int i = 0;i<3;i++){
-        for (int j=0;j<3;j++){
-            cout<<board[i][j]<<"\t";
+void printBoard(char board[][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << board[i][j] << "\t";
         }
-        cout<<endl;
+        cout << endl;
     }
+}
+
+bool isFreeSpace(char board[][3], string move)
+{
+    int row = move[0] - 'A';
+    int column = move[1] - '1';
+    if (board[row][column] == ' ')
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void registerMove(char board[][3], char player, string move)
+{
+    int row = move[0] - 'A';
+    int column = move[1] - '1';
+    board[row][column] = player;
 }
